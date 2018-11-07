5a9589511cb51f2794b835bb3c4b989a4768b0f3
Jose Cruz <jcruz@pidelorapido.com>
2018-11-07 03:35:17 -0600

    finished priego page

---
+++ main.js
@@ -0,0 +1,69 @@
+function galeryLeft(){
+    var changeThis;
+    var el = document.getElementById("galery-slider");
+    var children = el.children;
+    var helper = document.getElementById('galery-helper');
+    var helpChildren = helper.children;
+    for (var i = children.length - 1; i >= 0; i--){
+        if(changeThis){
+            changeThis = false;
+            children[i].classList.replace('hide', 'show');
+            helpChildren[i].classList.replace('hide', 'show');
+            break;
+        }
+        if(children[i].classList[0] == 'show'){
+            changeThis = true;
+            children[i].classList.replace('show', 'hide');
+            helpChildren[i].classList.replace('show', 'hide');
+            if(i == 0){
+                children[children.length - 1].classList.replace('hide','show');
+                helpChildren[children.length - 1].classList.replace('hide','show');
+            }
+        }
+    }
+}
+
+function galeryRight(){
+    console.log('right')
+    var changeThis;
+    var el = document.getElementById("galery-slider");   
+    var children = el.children;
+    var helper = document.getElementById('galery-helper');
+    var helpChildren = helper.children;
+    for (var i = 0; i < children.length; i++){
+        if(changeThis){
+            changeThis = false;
+            children[i].classList.replace('hide', 'show');
+            helpChildren[i].classList.replace('hide', 'show');
+            break;
+        }
+        if(children[i].classList[0] == 'show'){
+            changeThis = true;
+            children[i].classList.replace('show', 'hide');
+            helpChildren[i].classList.replace('show', 'hide');
+            if(i == children.length - 1){
+                children[0].classList.replace('hide','show');
+                helpChildren[0].classList.replace('hide','show');
+            }
+        }
+    }
+}
+
+function goToProject(projectNumber){
+    var projectHolder = document.getElementById('project-holder');
+    projectHolder.classList.add('proj-'+projectNumber);
+}
+
+function closeProject(){
+    var projectHolder = document.getElementById('project-holder');
+    projectHolder.removeAttribute("class");
+}
+
+function goToMenu(){
+    var menuHolder = document.getElementById('menu-holder');
+    menuHolder.classList.add('show-menu');
+}
+function closeMenu(){
+    var menuHolder = document.getElementById('menu-holder');
+    menuHolder.removeAttribute("class");
+}
