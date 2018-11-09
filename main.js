40ae25843d7186f1ee7dd43a092feb9179efb239
jose-cruz <josealejandrocruz.90@gmail.com>
2018-11-09 16:28:43 -0600

    cambios

--- main.js
+++ main.js
@@ -1,52 +1,83 @@
-function galeryLeft(){
+function galeryLeft(galleryName, useHelper){
     var changeThis;
-    var el = document.getElementById("galery-slider");
+    var el = document.getElementById(galleryName);
     var children = el.children;
-    var helper = document.getElementById('galery-helper');
-    var helpChildren = helper.children;
+    if(useHelper){
+        var helper = document.getElementById('galery-helper');
+        var helpChildren = helper.children;
+    }
     for (var i = children.length - 1; i >= 0; i--){
         if(changeThis){
             changeThis = false;
             children[i].classList.replace('hide', 'show');
-            helpChildren[i].classList.replace('hide', 'show');
+            if (useHelper) {
+                helpChildren[i].classList.replace('hide', 'show');
+            }
             break;
         }
         if(children[i].classList[0] == 'show'){
             changeThis = true;
             children[i].classList.replace('show', 'hide');
-            helpChildren[i].classList.replace('show', 'hide');
+            if(useHelper){
+                helpChildren[i].classList.replace('show', 'hide');
+            }
             if(i == 0){
                 children[children.length - 1].classList.replace('hide','show');
-                helpChildren[children.length - 1].classList.replace('hide','show');
+                if(useHelper){
+                    helpChildren[children.length - 1].classList.replace('hide','show');
+                }
             }
         }
     }
 }
 
-function galeryRight(){
-    console.log('right')
+function galeryRight(galleryName, useHelper) {
     var changeThis;
-    var el = document.getElementById("galery-slider");   
+    var el = document.getElementById(galleryName);
     var children = el.children;
-    var helper = document.getElementById('galery-helper');
-    var helpChildren = helper.children;
+    if (useHelper) {
+        var helper = document.getElementById('galery-helper');
+        var helpChildren = helper.children;
+    }
     for (var i = 0; i < children.length; i++){
         if(changeThis){
             changeThis = false;
             children[i].classList.replace('hide', 'show');
-            helpChildren[i].classList.replace('hide', 'show');
+            if (useHelper) {
+                helpChildren[i].classList.replace('hide', 'show');
+            }
             break;
         }
         if(children[i].classList[0] == 'show'){
             changeThis = true;
             children[i].classList.replace('show', 'hide');
-            helpChildren[i].classList.replace('show', 'hide');
+            if (useHelper) {
+                helpChildren[i].classList.replace('show', 'hide');
+            }
             if(i == children.length - 1){
                 children[0].classList.replace('hide','show');
-                helpChildren[0].classList.replace('hide','show');
+                if (useHelper) {
+                    helpChildren[0].classList.replace('hide','show');
+                }
             }
         }
     }
+}
+
+
+function helperSelector(number) {
+    var el = document.getElementById("galery-slider");
+    var children = el.children;
+    var helper = document.getElementById('galery-helper');
+    var helpChildren = helper.children;
+
+    for (var i = 0; i < children.length; i++) {
+        children[i].classList.replace('show', 'hide');
+        helpChildren[i].classList.replace('show', 'hide');
+    }
+
+    children[number].classList.replace('hide', 'show');
+    helpChildren[number].classList.replace('hide', 'show');
 }
 
 function goToProject(projectNumber){
@@ -67,3 +98,4 @@ function closeMenu(){
     var menuHolder = document.getElementById('menu-holder');
     menuHolder.removeAttribute("class");
 }
+
